23e01d00b1add390dfbeb7457e8ccb91240197b7
Kara Rawson <rawsonkara@gmail.com>
2021-02-25 17:06:49 -0500

    document exports interface

--- packages/core/src/util/IBenchExports.ts
+++ packages/core/src/util/IBenchExports.ts
@@ -6,17 +6,34 @@ type bool = 1 | 0;
  * provided by `as-bench` benchmark suite
  */
 export interface IBenchExports {
+  
   /** Call a function by it's table index. */
   __call(index: number): void;
+  
   /** The explicit start function. */
   _start(): void;
-
+  
+  /** return calculated value for mean average */
   __getDefaultCalculateMean(): bool;
+
+  /** return calculated value for median average */
   __getDefaultCalculateMedian(): bool;
+
+  /** returns calculated value for maximum result */
   __getDefaultCalculateMax(): bool;
+
+  /** returns calculated minimum result value */
   __getDefaultCalculateMin(): bool;
+
+  /** returns calculates average of mean differences */
   __getDefaultCalculateVariance(): bool;
+
+  /** returns calculated difference of mean and result */
   __getDefaultCalculateStdDev(): bool;
+
+  /** returns total bench call's */
   __getDefaultIterationCount(): number;
+
+  /** returns total bench execution time */
   __getDefaultMaxRuntime(): number;
 }
